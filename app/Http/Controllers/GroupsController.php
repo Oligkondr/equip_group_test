3960065ecb56893185496b8362af55313b9b3896
Kondrashin Oleg <oligkon@gmail.com>
2026-08-12 23:03:16 +0300

    Add pagination template

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Group;
+use App\Models\Product;
 
 class GroupsController extends Controller
 {
     public function __invoke(string $path = null)
     {
+        $order = request('order');
+        $size = request('size', 6);
+
         $path = array_filter(explode('/', $path));
 
         $groups = Group::query()
@@ -15,9 +19,21 @@ class GroupsController extends Controller
             ->where('id_parent', 0)
             ->get();
 
+        $query = Product::query();
+
+        if ($path) {
+            $current = Group::find(last($path));
+            $childrenIds = $current->getChildrenIds();
+
+            $query->whereIn('id_group', $childrenIds);
+        }
+
+        $products = $query->paginate($size);
+
         return view('groups', [
             'path' => $path,
             'groups' => $groups,
+            'products' => $products,
         ]);
     }
 }
--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -10,7 +10,17 @@
             </ul>
         </div>
         <div class="col-8">
-            adwadw
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col-md-4 mb-3">
+                        <div class="card p-3">{{ $product->name }}</div>
+                    </div>
+                @endforeach
+            </div>
+
+            <div class="d-flex justify-content-center mt-4">
+                {{ $products->links() }}
+            </div>
         </div>
     </div>
 @endsection
